27153743e8dcf623daa672376d06062bf86bc37c
BARRY Abdoulaye <barryab12@gmail.com>
2020-11-25 14:26:09 +0000

    Add voice2text

--- translator.py
+++ translator.py
@@ -1,8 +1,9 @@
 from gtts import gTTS
 import sys, getopt, os
+import speech_recognition as sr
 
 
-def translatorTTS(file):
+def text2voice(file):
     language = 'fr'
     fichier = open(file, "r")
     contenu = fichier.read()
@@ -11,6 +12,22 @@ def translatorTTS(file):
     speech.save("{}.mp3".format(file))
     print("Fichier audio enregisté avec succès")
     fichier.close()
+
+
+def voice2text(file):
+    filename = file
+    # initialize the recognizer
+    r = sr.Recognizer()
+    # open the file
+    with sr.AudioFile(filename) as source:
+        # listen for the data (load audio to memory)
+        audio_data = r.record(source)
+        # recognize (convert from speech to text)
+        text = r.recognize_google(audio_data, language="fr-FR")
+        text_file = open("{}.txt".format(file), "w+")
+        text_file.write(text)
+        print(text)
+        text_file.close()
 
 
 def main(argv):
@@ -30,7 +47,9 @@ def main(argv):
         elif opt in ("-o", "--otype"):
             outputtype = arg
     if outputtype == "v":
-        translatorTTS(inputfile)
+        text2voice(inputfile)
+    elif outputtype == "t":
+        voice2text(inputfile)
     else:
         print(
             "Aucun type de sortie choisi ou mauvais format choisi. Les choix sont v ou t. ex: '-o v'"
