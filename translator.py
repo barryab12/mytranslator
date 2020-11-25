e59b143f9b00b189b9e302d59c1981920ef53405
BARRY Abdoulaye <barryab12@gmail.com>
2020-11-25 13:31:19 +0000

    Add args and making function

--- translator.py
+++ translator.py
@@ -1,11 +1,23 @@
 from gtts import gTTS
 import os
+import sys
 language = 'fr'
 
-fichier = open("text.txt", "r")
-contenu = fichier.read()
-print(contenu)
-speech = gTTS(text=contenu, lang=language, slow=False)
-speech.save("text.mp3")
-print("Fichier audio enregisté avec succès")
-fichier.close()
+
+def translator(file):
+    fichier = open(file, "r")
+    contenu = fichier.read()
+    print(contenu)
+    speech = gTTS(text=contenu, lang=language, slow=False)
+    speech.save("{}.mp3".format(file))
+    print("Fichier audio enregisté avec succès")
+    fichier.close()
+
+
+if __name__ == "__main__":
+    file = int(sys.argv[1])
+    output = int(sys.argv[2])
+    if output == "v":
+        translator(file)
+    else:
+        print("Ajouter le second paramètre")
